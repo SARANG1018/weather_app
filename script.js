1161cb6990216bb51bb0081542b7077f29345f83
Sarang Kadakia <sarangkadakia10@gmail.com>
2023-03-19 12:30:10 +0530

    weather_app

---
+++ script.js
@@ -0,0 +1,37 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f64444d350msh275d8516da15090p151254jsn0fb4fc244a2d',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+            // cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            temp2.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            humidity2.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_speed2.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+
+        })
+        .catch(err => console.error(err));
+}
+submit.addEventListener('click', (e) => {
+    getWeather(city.value);
+    e.preventDefault()
+    //e.preventDefault()- this function doesnt allow the page to reload
+})
+
+getWeather("Delhi")
